e686a1a82275b369f1987e7913b50b81f20acbba
Daniel <hernandezpdaniele@gmail.com>
2020-05-09 23:42:40 -0400

    feat: testing theme toggle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Observable} from 'rxjs';
+import {ThemeService} from './core/services/theme.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'EE-template';
+  isDarkTheme: Observable<boolean>;
+
+  constructor(private themeService: ThemeService) {
+  }
+
+  ngOnInit() {
+    this.isDarkTheme = this.themeService.isDarkTheme;
+  }
+
+  toggleDarkTheme(checked: boolean) {
+    this.themeService.setDarkTheme(checked);
+  }
 }
