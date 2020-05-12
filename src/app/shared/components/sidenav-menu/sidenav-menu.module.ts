917230a750422bc0617261e3ab7b9fa90ee85d4f
Daniel <hernandezpdaniele@gmail.com>
2020-05-12 12:53:21 -0400

    feat: sidenav-menu empty component

---
+++ src/app/shared/components/sidenav-menu/sidenav-menu.module.ts
@@ -0,0 +1,13 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {SidenavMenuComponent} from './sidenav-menu.component';
+
+
+@NgModule({
+  declarations: [SidenavMenuComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class SidenavMenuModule {
+}
