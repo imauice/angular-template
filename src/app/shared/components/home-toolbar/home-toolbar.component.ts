42c455d3b9743adbaaf40ea27b5dc9203e816860
Daniel <hernandezpdaniele@gmail.com>
2020-05-12 12:51:58 -0400

    feat: home-toolbar empty component

---
+++ src/app/shared/components/home-toolbar/home-toolbar.component.ts
@@ -0,0 +1,16 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-home-toolbar',
+  templateUrl: './home-toolbar.component.html',
+  styleUrls: ['./home-toolbar.component.scss']
+})
+export class HomeToolbarComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/shared/components/home-toolbar/home-toolbar.module.ts
@@ -0,0 +1,13 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {HomeToolbarComponent} from './home-toolbar.component';
+
+
+@NgModule({
+  declarations: [HomeToolbarComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class HomeToolbarModule {
+}
