4952d892a3670410405a9d9b2293b0472c31e97d
Daniel Stefanelli <daniel.stefanelli.h@gmail.com>
2020-07-02 20:43:56 -0400

    feat: home toolbar component

--- src/app/pages/home/home-toolbar/home-toolbar.component.ts
+++ src/app/pages/home/home-toolbar/home-toolbar.component.ts
@@ -10,21 +10,15 @@ import {ThemeService} from '../../../core/services/theme.service';
 export class HomeToolbarComponent implements OnInit {
   @Output() toggle = new EventEmitter<void>();
   @Output() signOut = new EventEmitter<void>();
-  @Input() githubId: string;
   @Input() name: string;
   @Input() isMobile: boolean;
   isDarkTheme: Observable<boolean>;
-  color = 'primary';
 
   constructor(private themeService: ThemeService) {
   }
 
   ngOnInit() {
     this.isDarkTheme = this.themeService.isDarkTheme;
-
-    this.themeService.isDarkTheme.subscribe(value => {
-      this.color = value ? 'none' : 'primary';
-    });
   }
 
   toggleDarkTheme(checked: boolean) {
