93d12e77d3020f6780590f91b4106d9e5e81894b
Daniel Stefanelli <daniel.stefanelli.h@gmail.com>
2020-08-02 11:58:48 -0400

    refactor: project renamed.

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/EE-template'),
+      dir: require('path').join(__dirname, './coverage/angular-template'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
