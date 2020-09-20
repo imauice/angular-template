fcefa2d4eb450c36ea6fa14101d52c19c4d89f4f
Daniel Stefanelli <daniel.stefanelli.h@gmail.com>
2020-09-20 10:10:08 -0400

    style: subscribe to theme observable to change background header color when dark theme is active.

--- src/app/shared/directives/template-tables/template-header-row.directive.ts
+++ src/app/shared/directives/template-tables/template-header-row.directive.ts
@@ -1,4 +1,5 @@
 import {Directive, ElementRef, Renderer2} from '@angular/core';
+import {ThemeService} from '../../../core/services/theme.service';
 
 @Directive({
   // tslint:disable-next-line:directive-selector
@@ -6,9 +7,14 @@ import {Directive, ElementRef, Renderer2} from '@angular/core';
 })
 export class TemplateHeaderRowDirective {
 
-  constructor(private el: ElementRef, private renderer: Renderer2) {
+  constructor(private el: ElementRef,
+              private renderer: Renderer2,
+              private themeService: ThemeService) {
     this.renderer.setStyle(this.el.nativeElement, 'border', 'none');
     this.renderer.setStyle(this.el.nativeElement, 'background-color', '#F4F4F4');
+    this.themeService.isDarkTheme.subscribe(darkTheme => {
+      this.renderer.setStyle(this.el.nativeElement, 'background-color', '#212121');
+    });
   }
 
 }
