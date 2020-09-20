2631733ccd2a52af18489bdcd21db4ca255aadf1
Daniel Stefanelli <daniel.stefanelli.h@gmail.com>
2020-09-20 12:40:15 -0400

    Auto-generated commit

---
+++ 10-es2015.d447c524d881d2b70dc4.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[10],{hAKZ:function(n,o,t){"use strict";t.r(o),t.d(o,"SignModule",(function(){return u}));var r=t("ofXK"),e=t("tyNb"),c=t("PCNd"),i=t("fXoL");const s=[];let u=(()=>{class n{}return n.\u0275mod=i.Tb({type:n}),n.\u0275inj=i.Sb({factory:function(o){return new(o||n)},imports:[[r.c,e.e.forChild(s),c.a]]}),n})()}}]);
